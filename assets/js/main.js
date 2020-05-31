d8efdb456d8ecc3c300eb833ee68d1456cd13ca9
virnarula <virsvirus@gmail.com>
2020-05-31 15:39:50 -0500

    Beatify my github section

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,7 +63,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "cmsparks");
+    new GitHubCalendar("#github-graph", "virnarula");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
